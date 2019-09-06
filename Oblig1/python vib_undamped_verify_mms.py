2b71011334f1ad37e00610b2bc62c5be85ceae5e
Marte Fossum <martegnom@gmail.com>
2019-09-06 11:02:35 +0200

    Ny mappe

--- Oblig1/python vib_undamped_verify_mms.py
+++ Oblig1/python vib_undamped_verify_mms.py
@@ -1,6 +1,7 @@
 import sympy as sym
 V, t, I, w, dt = sym.symbols('V t I w dt')  # global symbols
 f = None  # global variable for the source term in the ODE
+B = 1
 
 
 def ode_source_term(u):
@@ -12,15 +13,16 @@ def ode_source_term(u):
 
 def residual_discrete_eq(u, dt):
     """Return the residual of the discrete eq. with u inserted."""
-    R = u - ode_source_term(u)
+    #f = ode_source_term(u)
+    R = u(t + dt) - w**2*u(t) + f + 2*u(t) - u(t - dt)
     return sym.simplify(R)
 
 
 def residual_discrete_eq_step1(u, dt):
     """Return the residual of the discrete eq. at the first
     step with u inserted."""
-    f = ode_source_term(u)
-    R = u(dt) - f.subs(t, 0)*
+    #f = ode_source_term(u)
+    R = u(dt) - (f.subs(t, 0)*dt**2 - w*I*dt**2 + V*dt + I)
     return sym.simplify(R)
 
 
@@ -39,6 +41,8 @@ def main(u):
     to compute the source term f, and check if u also solves
     the discrete equations.
     """
+    N = 1000
+    dt = 1/(N+1)
     print('=== Testing exact solution: %s ===' % u)
     print("Initial conditions u(0)=%s, u'(0)=%s:" %
           (u(t).subs(t, 0), sym.diff(u(t), t).subs(t, 0)))
@@ -48,23 +52,18 @@ def main(u):
     f = sym.simplify(ode_source_term(u))
 
     # Residual in discrete equations (should be 0)
-    print('residual step1:', residual_discrete_eq_step1(u))
-    print('residual:', residual_discrete_eq(u))
-
-
-def solver(u):
-
-    return ans
+    print('residual step1:', residual_discrete_eq_step1(u, dt))
+    print('residual:', residual_discrete_eq(u, dt))
 
 
 def linear():
     main(lambda t: V*t + I)
 
 
-def quadratic(B):
+def quadratic():
     main(lambda t: B*t**2 + V*t + I)
 
 
 if __name__ == '__main__':
     linear()
-    quadratic()
+    # quadratic()
